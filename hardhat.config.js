2fe3e6086bbad79568a0a7ea5306ceee0bc5599a
Aeshna0204 <97512618+Aeshna0204@users.noreply.github.com>
2025-01-07 11:26:58 +0530

    Update hardhat.config.js
    Signed-off-by: Aeshna0204 <97512618+Aeshna0204@users.noreply.github.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     edu: {
       url: "https://rpc.open-campus-codex.gelato.digital/", // Replace with the RPC URL of the EDU blockchain
-      accounts: [`0xd526f09224fc1ebb554f49812d0a0b86987fc3d7cba71ec4d358cc73bf4a5d06`], // Replace with your private key
+      accounts: [``], // Replace with your private key
     },
   },
   
